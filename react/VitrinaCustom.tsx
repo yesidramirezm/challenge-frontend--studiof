cc08ff872135947de5dfc651f1e4646debc37017
Yesid Ramirez Mendez <yesidramirezmendez@gmail.com>
2025-06-03 08:44:56 -0500

    estylos y custom react

--- react/VitrinaCustom.tsx
+++ react/VitrinaCustom.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import useProduct from 'vtex.product-context/useProduct'
+import { useOrderItems } from 'vtex.order-items/OrderItems';
+
+
+interface Propagations {
+  stopPropagation: () => void;
+  preventDefault: () => void
+}
+
 const VitrinaCustom = () => {
- const {product} = useProduct()
- console.log(product,"data")
+  const { selectedItem: { complementName, itemId, sellers } } = useProduct()
+
+  const { addItems } = useOrderItems();
+
+  const addToCart = (e: Propagations) => {
+    e.stopPropagation();
+    e.preventDefault()
+
+    const itemToAdd = [{
+      id: itemId,
+      quantity: 1,
+      seller: sellers[0].sellerId
+    }]
+
+    addItems(itemToAdd);
+  };
 
   return (
-    <div>
-      ref
+    <div >
+      <button onClick={addToCart}>
+        <p>{complementName}</p>
+
+        <p>{itemId}</p>
+      </button>
     </div>
   )
 }
